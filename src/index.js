6e7e850d09ae9ca086f797b26953c638b94cc805
Erin Li <lihuiying2002@outlook.com>
2024-01-17 16:10:37 -0800

    used graphql

--- src/index.js
+++ src/index.js
@@ -7,19 +7,29 @@ import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'https://crwn-clothing.com/',
+  cache: new InMemoryCache()
+})
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>    
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <UserProvider>
+          <CategoriesProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CategoriesProvider>
+        </UserProvider>
+      </BrowserRouter>    
+    </ApolloProvider>
+    
   </React.StrictMode>
 );
 
--- src/routes/category/category.jsx
+++ src/routes/category/category.jsx
@@ -3,16 +3,43 @@ import { useContext, useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { CategoriesContext } from '../../contexts/categories.context';
 import ProductCard from '../../components/product-card/product-card';
-
+import {gql, useQuery} from '@apollo/client'
 import {CategoryContainer, Title} from './category.styles';
+
+const GET_CATEGORY = gql`
+  query($title: String!) {
+    getCollectionsByTitle(title: $title) {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`
 
 const Category = () => {
   const { category } = useParams()
-  const { categoriesMap } = useContext(CategoriesContext)
-  const [products, setProducts] = useState(categoriesMap[category])
-  useEffect(() => {
-    setProducts(categoriesMap[category])
-  }, [category, categoriesMap])
+  const { loading, error, data} = useQuery(GET_CATEGORY, {
+    variables: {
+      title: category
+    }
+  })
+
+  useEffect(()=> {
+    if(data) {
+      const {
+        getCollectionsByTitle: {items}
+      } = data
+      setProducts(items)
+    }
+  },[category, data])
+
+  const [products, setProducts] = useState([])
+
   return (
     <>
       <Title>{category.toUpperCase()}</Title>
